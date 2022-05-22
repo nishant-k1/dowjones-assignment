d79f909d899bfed3583bad6fe502452a0184def6
Nishant Kumar <nishant290195.nkr@gmail.com>
2022-05-22 08:18:32 +0530

    playing with navbar style

--- src/views/layouts/Header/index.js
+++ src/views/layouts/Header/index.js
@@ -2,42 +2,78 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './index.module.css';
 import { RiArrowDropDownLine } from 'react-icons/ri';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 const Header = () => {
   return (
-    <div className={styles.section}>
-      <div className={styles.container}>
-        <div className={styles.startItems}>
-          <Link className={styles.link} to={`/posts?page=${0}`}>
-            ABOUT
-          </Link>
-          <Link className={styles.link} to="/create">
-            ADVERTISE WITH US
-          </Link>
-        </div>
-
-        <div className={styles.midItems}>
-          <div>
+    <React.Fragment>
+      <div className={styles.section}>
+        <div className={styles.container}>
+          <div className={styles.startItems}>
             <Link className={styles.link} to={`/posts?page=${0}`}>
-              ENGLISH
+              ABOUT
             </Link>
-            <RiArrowDropDownLine className={styles.icon} />
-          </div>
-          <div>
             <Link className={styles.link} to="/create">
-              $ USD
+              ADVERTISE WITH US
             </Link>
-            <RiArrowDropDownLine className={styles.icon} />
           </div>
-        </div>
 
-        <div className={styles.endItem}>
-          <Link className={styles.link} to="/create">
-            SIGN IN
-          </Link>
+          <div className={styles.midItems}>
+            <div>
+              <Link className={styles.link} to={`/posts?page=${0}`}>
+                ENGLISH
+              </Link>
+              <RiArrowDropDownLine className={styles.icon} />
+            </div>
+            <div>
+              <Link className={styles.link} to="/create">
+                $ USD
+              </Link>
+              <RiArrowDropDownLine className={styles.icon} />
+            </div>
+          </div>
+
+          <div className={styles.endItem}>
+            <Link className={styles.link} to="/create">
+              SIGN IN
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+      <div className={styles.section}>
+        <div className={`${styles.container} ${styles.midHeader}`}>
+          <h1>
+            mansion <span>global</span>
+          </h1>
+          <div>
+            <Link className={styles.link} to={`/posts?page=${0}`}>
+              NEWS
+            </Link>
+            <Link className={styles.link} to={`/posts?page=${0}`}>
+              TOP MARKETS
+            </Link>
+            <Link className={styles.link} to={`/posts?page=${0}`}>
+              NEW DEVELOPMENTS
+            </Link>
+            <Link className={styles.link} to={`/posts?page=${0}`}>
+              MORE
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles.section}>
+        <div className={`${styles.container} ${styles.bottomHeader}`}>
+          <div>
+            <select name="Buy" id="1">
+              <option value="1">Buy</option>
+              <option value="2">Rent</option>
+            </select>
+            <p>select a location</p>
+            <AiOutlineSearch className={styles.icon} />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
