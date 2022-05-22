9ffc697a93b5315e709843d9332673fd84866b4e
Nishant Kumar <nishant290195.nkr@gmail.com>
2022-05-23 05:29:33 +0530

    playing with mobile view nav  menus

--- src/views/layouts/Header/MobileNav.js
+++ src/views/layouts/Header/MobileNav.js
@@ -2,16 +2,51 @@ import React from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import styles from './MobileNav.module.css';
+import { Link } from 'react-router-dom';
 
-const MobileNav = () => {
+const MobileNavMenus = ({ setShowMenu }) => {
   return (
     <div className={styles.section}>
-      <div className={styles.container}>
-        <GiHamburgerMenu className={styles.icon} />
-        <h1>Mansion Global</h1>
-        <AiOutlineSearch className={styles.icon} />
+      <div className={`${styles.container} ${styles.menus}`}>
+        <Link
+          to="/posts"
+          onClick={() => {
+            setShowMenu(false);
+          }}
+        >
+          Home
+        </Link>
+        <Link
+          to="/create"
+          onClick={() => {
+            setShowMenu(false);
+          }}
+        >
+          Create Post
+        </Link>
       </div>
     </div>
+  );
+};
+
+const MobileNav = () => {
+  const [showMenu, setShowMenu] = React.useState(false);
+
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <React.Fragment>
+      <div className={styles.section}>
+        <div className={styles.container}>
+          <GiHamburgerMenu className={styles.icon} onClick={handleClick} />
+          <h1>Mansion Global</h1>
+          <AiOutlineSearch className={styles.icon} />
+        </div>
+      </div>
+      {showMenu && <MobileNavMenus setShowMenu={setShowMenu} />}
+    </React.Fragment>
   );
 };
 
