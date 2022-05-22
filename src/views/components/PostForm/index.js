087998b6317072a383d13cda1058bf2879d6f7dd
Nishant Kumar <nishant290195.nkr@gmail.com>
2022-05-23 00:00:21 +0530

    playing with create post

--- src/views/components/PostForm/index.js
+++ src/views/components/PostForm/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './index.module.css';
 
 import { formUtils } from 'views/utils/constants';
 
@@ -6,27 +7,41 @@ const PostForm = ({ value, handleClick, onChange }) => {
   const { userId, title, article, submit } = formUtils;
 
   return (
-    <div>
-      <label htmlFor="userId">{userId}</label>
-      <input
-        type="text"
-        name="userId"
-        value={value.userId}
-        onChange={onChange}
-      />
-      <label htmlFor="title">{title}</label>
-      <input type="text" name="title" value={value.title} onChange={onChange} />
-      <label htmlFor="article">{article}</label>
-      <textarea
-        name="article"
-        value={value.article}
-        onChange={onChange}
-        rows="4"
-        cols="50"
-      />
-      <button onClick={handleClick} type="submit">
-        {submit}
-      </button>
+    <div className={styles.section}>
+      <div className={styles.container}>
+        <h3>Create A Post</h3>
+        <div>
+          <label htmlFor="userId">{userId}</label>
+          <input
+            type="text"
+            name="userId"
+            value={value.userId}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="title">{title}</label>
+          <input
+            type="text"
+            name="title"
+            value={value.title}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="article">{article}</label>
+          <textarea
+            name="article"
+            value={value.article}
+            onChange={onChange}
+            rows="4"
+            cols="50"
+          />
+        </div>
+        <button onClick={handleClick} type="submit">
+          {submit}
+        </button>
+      </div>
     </div>
   );
 };
