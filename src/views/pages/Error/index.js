bb20bb6b83805a6cd34011d20992fb4ec048f832
Nishant Kumar <nishant290195.nkr@gmail.com>
2022-05-23 21:56:31 +0530

    design error page

--- src/views/pages/Error/index.js
+++ src/views/pages/Error/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './index.module.css';
 
 const Error = () => {
   return (
     <React.Fragment>
-      <div>Error 404</div>
-      <div>The page you are looking for could not be found</div>
-      <Link to="/">Go to the home page</Link>
+      <div className={styles.section}>
+        <div className={styles.container}>
+          <h1>Error 404</h1>
+          <hr />
+          <h2>The page you are looking for could not be found</h2>
+          <Link to="/">Go to the home page</Link>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
